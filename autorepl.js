df6767556678e2f69a981d0ec0ecc3873921ca62
Bryan Dina <bdina@dyn.com>
2013-04-29 12:25:54 -0400

    first version of the autorepl program.

---
+++ autorepl.js
@@ -0,0 +1,33 @@
+#! /usr/local/bin/node
+
+var repl  = require('repl');
+var fs    = require('fs');
+var watch = require('node-watch');
+var path  = require('path');
+var proc  = require('child_process');
+
+/** Functions */
+
+function staticAnalysis(file) {
+  console.log('detected event on file [' + file + ']\n');
+  fs.exists(file, function(exists) {
+    if ( exists ) {
+      proc.exec('/usr/local/bin/jshint ' + file, function(err, stdout, stderr) { console.log(stdout); });
+    } else {
+      console.log('file was DESTROYED');
+    }
+  });
+}
+
+function filter(regex, fn) {
+  return function(file) { if (regex.test(file)) { fn(file); } }
+}
+
+function js_filter(fn) { return filter(/\.js$/, fn) }
+
+/** Now for the logic */
+
+watch(/* current dir */ path.resolve(__dirname),
+      /* listener    */ js_filter(staticAnalysis));
+
+repl.start(" >> ", null, null, null, true);
