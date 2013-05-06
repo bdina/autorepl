87a216149ec96718416fbc1cecf5a4022a18e33e
Bryan Dina <bdina@dyn.com>
2013-05-06 17:18:05 -0400

    swapped out node-watcher with fs-watch-tree because it supports excludes

--- autorepl.js
+++ autorepl.js
@@ -1,52 +1,40 @@
 #! /usr/local/bin/node
 
 // Standard Node.js Library Require
-var repl  = require('repl');
-var fs    = require('fs');
-var path  = require('path');
-var proc  = require('child_process');
+var repl = require('repl');
+var fs   = require('fs');
+var path = require('path');
+var proc = require('child_process');
 
 // 3rd Party Library Require
-var watch = require('node-watch');
+var watcher = require('fs-watch-tree');
 
 /** Functions */
 
-function staticAnalysis(file) {
+function lint_and_notify(e) {
   var handleJshint = function(err, stdout, stderr) {
     console.log(stdout);
     if ( err === null ) {
-        console.log('No errors found.')
+        console.log('No errors found.');
         proc.exec('growlnotify -name "JSHint" -m "Linting found no errors" Success');
         proc.exec('/usr/local/bin/ctags -f ' + process.cwd() + '/tags .');
     } else {
-        console.log('Errors found!')
         proc.exec('growlnotify -name "JSHint" -m "Linting found errors" Failure');
     }
   };
 
-  console.log('detected event on file [' + file + ']\n');
-  fs.exists(file, function(exists) {
-    if ( exists ) {
-      proc.exec('jshint ' + file, handleJshint);
-    } else {
-      console.log('file was DESTROYED');
-    }
-  });
-}
+  console.log('detected event on file [' + e.name + ']');
 
-// Function called by the watch code to determine if the file needs watching
-function filter(incl, excl, fn) {
-  return function(file) { if (!excl.test(file) && incl.test(file)) { fn(file); } };
+  if ( e.isModify() && !e.isDirectory() ) {
+    proc.exec('jshint ' + e.name, handleJshint);
+  }
 }
-
-// Composing the filter
-function incl() { return /\.js$/; }
-function excl() { return /(node_modules)/; }
 
 /** Now for the logic */
 
 console.log('Starting to watch ' + process.cwd());
-watch(/* current dir */ process.cwd(),
-      /* listener    */ filter(incl(), excl(), staticAnalysis));
+watcher.watchTree(/* current dir */ process.cwd(),
+                  /* options     */ { exclude: [ 'node_modules', '.git', /^((?!.js).)*$/ ] },
+                  /* listener    */ lint_and_notify);
 
 repl.start(" >> ", null, null, null, true);
